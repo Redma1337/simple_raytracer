f86cfa9db99e23afd7eb23da3de2c461291ff2bb
Redma1337 <battlefield.noob66@gmail.com>
2023-11-13 15:11:22 +0100

    feat: first commit

---
+++ simple_raytracer/raytracer/scene/shapes/Sphere.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class Sphere
+{
+public:
+    
+};
---
+++ simple_raytracer/utils/vector3.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+class Vector3
+{
+public:
+    int x, y, z;
+
+    Vector3(const int x, const int y, const int z)
+        : x{x}, y{y}, z{z}
+    {}
+
+    
+};
