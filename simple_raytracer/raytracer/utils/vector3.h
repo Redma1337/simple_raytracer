6f851b3019db06a72176a008a081ac33c4768f93
Redma1337 <battlefield.noob66@gmail.com>
2023-12-12 14:51:15 +0100

    feat: vector utils functions

--- simple_raytracer/raytracer/utils/vector3.h
+++ simple_raytracer/raytracer/utils/vector3.h
@@ -12,35 +12,35 @@ public:
     Vector3(const float x, const float y, const float z) : x(x), y(y), z(z) {}
 
     Vector3 operator+(const Vector3& other) const {
-        return Vector3{x + other.x, y + other.y, z + other.z};
+        return {x + other.x, y + other.y, z + other.z};
     }
 
     Vector3 operator-(const Vector3& other) const {
-        return Vector3{x - other.x, y - other.y, z - other.z};
+        return {x - other.x, y - other.y, z - other.z};
     }
 
     Vector3 operator*(const Vector3& other) const {
-        return Vector3{x * other.x, y * other.y, z * other.z};
+        return {x * other.x, y * other.y, z * other.z};
     }
 
     Vector3 operator/(const Vector3& other) const {
-        return Vector3{x / other.x, y / other.y, z / other.z};
+        return {x / other.x, y / other.y, z / other.z};
     }
 
     Vector3 operator*(const float scalar) const {
-        return Vector3{x * scalar, y * scalar, z * scalar};
+        return {x * scalar, y * scalar, z * scalar};
     }
 
     Vector3 operator/(const float scalar) const {
-        return Vector3{x / scalar, y / scalar, z / scalar};
+        return {x / scalar, y / scalar, z / scalar};
     }
 
     Vector3 operator-(const float scalar) const {
-        return Vector3{x - scalar, y - scalar, z - scalar};
+        return {x - scalar, y - scalar, z - scalar};
     }
     
     Vector3 operator+(const float scalar) const {
-        return Vector3{x + scalar, y + scalar, z + scalar};
+        return {x + scalar, y + scalar, z + scalar};
     }
 
     float dot(const Vector3& other) const {
@@ -48,16 +48,51 @@ public:
     }
 
     Vector3 cross(const Vector3& other) const {
-        return Vector3(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
+        return {y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x};
     }
 
-    Vector3 normalize() const {
-        const float magnitude = math::qsqrt(x * x + y * y + z * z);
+    float length() const {
+        return math::qsqrt(x * x + y * y + z * z);
+    }
 
+    static Vector3 reflect(const Vector3& v, const Vector3& n) {
+        return v - n * 2 * v.dot(n);
+    }
+    
+    Vector3 normalize() const {
+        const float magnitude = length();
         if (magnitude != 0.f) {
             return{ x / magnitude, y / magnitude, z / magnitude };
         }
         
         return { };
+    }
+
+    static Vector3 random_on_hemisphere(const Vector3& normal) {
+        Vector3 on_unit_sphere = random_unit_vector();
+        if (on_unit_sphere.dot(normal) > 0.0)
+            return on_unit_sphere;
+
+        return on_unit_sphere * -1;
+    }
+
+    static Vector3 random_unit_vector()
+    {
+        while (true)
+        {
+            auto rng_v = random_vector(-1, 1);
+            if (rng_v.length() < 1) {
+                return rng_v.normalize();
+            }
+        }
+    }
+
+    static Vector3 random_vector(const float min, const float max)
+    {
+        return {
+            math::random_float(min,max),
+            math::random_float(min,max),
+            math::random_float(min,max)
+        };
     }
 };
