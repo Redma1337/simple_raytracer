fe3c5b1c790a8939926bea3dee947fd4f4c36711
Redma1337 <battlefield.noob66@gmail.com>
2023-12-12 13:32:02 +0100

    feat: use sphere with high radius as ground

--- simple_raytracer/main.cpp
+++ simple_raytracer/main.cpp
@@ -11,9 +11,9 @@ int main()
 {
     Raytracer tracer(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    tracer.add_sphere({ -4, -4, -10 }, 3, { 1, 0, 0 });
-    tracer.add_sphere({ 0, 1, -8 }, 3, { 0, 1, 0 });
-    //tracer.add_sphere({ 0, 100.5f, 0 }, 100, { 1, 1, 1 });
+    tracer.add_sphere({ 0, 100.5f, -1 }, 100, { 1, 1, 1 });
+    tracer.add_sphere({ 0, 0, -1 }, 0.5f, { 1, 0, 0 });
+    tracer.add_sphere({ -0.8, -0.4f, -1 }, 0.4f, { 0, 1, 0 });
     tracer.add_light({ -2, 1, -4 });
 
     tracer.start_rendering();
